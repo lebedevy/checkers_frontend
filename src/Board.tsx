7048443dd8310e8b82e3bfd891d39b612bc35612
Yury Lebedev <y.d.lebedev@gmail.com>
2023-03-15 17:54:28 -0400

    sign in wip

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,18 +1,51 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { css } from '@emotion/css';
 import { BlackCell, Cell } from './Cell';
 import { BOARD_SIZE } from './constants';
 import { DebugBoardSetting } from './DebugBoardSetting';
 import { useStore } from './storeContext';
+import { SignInModule } from './SignInModule';
+import { BigButton } from './BigButton';
+
+// 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+// 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
 
 export const Board: React.FC = () => {
     const [move, setMove] = useState<number>();
     const turn = useStore((s) => s.turn);
+    const setSigner = useStore((s) => s.signIn);
+    const signer = useStore((s) => s.signer);
+    const [signIn, setSignIn] = useState(false);
+
+    const updateBoard = useStore((s) => s.updateBoard);
+    const provider = useStore((s) => s.provider);
+
+    const updateListener = useCallback(() => {
+        console.log('block');
+        updateBoard();
+    }, [updateBoard]);
+
+    useEffect(() => {
+        console.log(provider);
+        console.log(updateListener);
+        provider?.on('block', updateListener);
+        // return () => {
+        //     provider?.off('block', updateListener);
+        // };
+    }, [provider, updateListener]);
 
     const squares = useMemo(() => Array(BOARD_SIZE * BOARD_SIZE).fill(0), []);
 
     return (
         <div>
+            {!signer && (
+                <BigButton
+                    title="Sign in"
+                    onClick={() => {
+                        setSignIn(true);
+                    }}
+                />
+            )}
             <div>{`${turn}`}</div>
             {squares
                 .reduce((prev, _, ind) => {
@@ -50,6 +83,16 @@ export const Board: React.FC = () => {
                     </div>
                 ))}
             <DebugBoardSetting />
+            {signIn && (
+                <SignInModule
+                    close={(signer) => {
+                        if (signer) {
+                            setSigner(signer);
+                        }
+                        setSignIn(false);
+                    }}
+                />
+            )}
         </div>
     );
 };
